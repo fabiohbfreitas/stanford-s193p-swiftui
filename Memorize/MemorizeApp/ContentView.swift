ec020bfe22d518e35531bbd393608f58229326f7
Fabio Freitas <fabiohbritofreitas@protonmail.com>
2025-08-05 14:51:45 -0300

    feat: workspace + empty swiftui project

---
+++ Memorize/MemorizeApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Fabio Freitas on 05/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
